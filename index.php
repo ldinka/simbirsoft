b2a8b59786d8f9bb572ad310a4b85a4b3786bd43
ldinka <icedinka@gmail.com>
2011-06-03 04:39:53 +0400

    regexp experiments; add preloader; the chunked loading for article file etc

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-<meta http-equiv="content-type" content="text/html; charset=windows-1251" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8" />
 <style>
     html, body {
         font: 12px/16px Arial;
@@ -21,6 +21,16 @@
     }
     b i, i b {
         background: #FF9393;
+    }
+    .link01 {
+        background: #eee;
+        display: inline-block;
+        margin: 5px;
+        padding: 5px;
+        text-align: center;
+    }
+    #preloader {
+        display: none;
     }
 </style>
 
@@ -43,16 +53,69 @@ setlocale(LC_ALL,"ru_RU.UTF-8");
 
 class Article
 {
+    const HEADER = '
+        <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+        <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
+        <head>
+        <meta http-equiv="content-type" content="text/html; charset=UTF-8" />
+        <style>
+            html, body {
+                font: 12px/16px Arial;
+            }
+            h1 {
+                color: #00438F;
+                font-size: 16px;
+                padding: 20px 40px;
+            }
+            label {
+                font-size: 14px;
+                display: inline-block;
+                width: 300px;
+            }
+            p {
+                margin: 6px 0;
+            }
+            b i, i b {
+                background: #FF9393;
+            }
+        </style>
+
+        </head>
+        <body>
+    ';
+    const FOOTER = '
+        </body>
+        </html>
+    ';
+
     private $article_text_array = array();
     private $result_article     = array();
+    private $number_of_strings;
 
     function __construct($article_file_path)
     {
         if (file_exists($article_file_path) && is_readable($article_file_path))
         {
+            $file_size = filesize($article_file_path);
             $article_file_resource = fopen($article_file_path, 'r');
-            $article_text = fread($article_file_resource, filesize($article_file_path));
-            $article_text_array = explode(".", $article_text);
+            $part_size = 0;
+            if ($file_size > 32768)
+                do
+                {
+                    $article_text = fread($article_file_resource, 32768);
+                    $temp_string = mb_strrchr($article_text, '.');
+                    if ($temp_string && $temp_string != '.')
+                        $temp_string = mb_strrchr($article_text, '.', true).'.';
+                    elseif (mb_strrchr($article_text, ' ') && mb_strrchr($article_text, ' ') != ' ')
+                        $temp_string = mb_strrchr($article_text, ' ', true).' ';
+                    else
+                        $temp_string = $article_text;
+                    $part_size += mb_strlen($temp_string);
+                    fseek($article_file_resource, $part_size);
+                    $article_text_array[] = $temp_string;
+                }
+                while ($part_size < $file_size);
+            else $article_text_array[] = fread($article_file_resource, $file_size);
             fclose($article_file_resource);
             $this->article_text_array = $article_text_array;
         }
@@ -60,21 +123,23 @@ class Article
             echo "Article file is not readable!";
     }
 
+    public function setNumberOfStrings($n)
+    {
+        $this->number_of_strings = $n;
+    }
+
+    public function getNumberOfStrings()
+    {
+        return $this->number_of_strings;
+    }
+
     public function processing($dictionary_text_array)
     {
-        $before = '(^|[^\p{L}])';
-        $after = '([^\p{L}]|$)';        
-
-
         foreach ($this->article_text_array as $key => $string)
             foreach ($dictionary_text_array as $word)
             {
+                //[\p{L}\p{Nd}]
                 $pattern = '~(^|[^\p{L}_])('.$word.')([^\p{L}_]|$)~ui';
-                //$pattern = '~(?<!\p{L})('.$word.')(?!\p{L})~u';
-
-                //preg_match_all($pattern, $string, $matches);
-                //f_print_r($matches);
-
                 $string = preg_replace($pattern, "\\1<b><i>\\2</i></b>\\3" , $string);
                 $result_article[$key] = $string;
             }
@@ -83,11 +148,21 @@ class Article
 
     private function printing ($article_result_array)
     {
-        foreach ($article_result_array as $string)
+        $result_string = implode("", $article_result_array);
+        $result_string = str_replace("\n", "<br/>\n", $result_string);
+        $main_array = explode("\n", $result_string);
+        $chunks = array_chunk($main_array, $this->number_of_strings);
+        echo "<p>Pages: ";
+        foreach ($chunks as $key => $chunk)
         {
-            $string = str_replace("\n", "<br/>\n", $string);
-            echo "<p>".$string."</p>\n";
+            $file_name = "./files00/".($key+1).".html";
+            $resource_html_file = fopen($file_name, "w");
+            fwrite($resource_html_file, $this::HEADER . implode("\n", $chunk) . $this::FOOTER);
+            fclose($resource_html_file);
+            chmod($file_name, 0666);
+            echo ' <a target="_blank" href="files00/'.($key+1).'.html" class="link01">'.($key+1).'</a> ' ;
         }
+        echo "</p>";
     }
 }
 
@@ -117,11 +192,11 @@ class Dictionary
             foreach ($dictionary_array as $word)
             {
                 $word = trim($word);
-                if (preg_match("~\s~", $word))
+                if (preg_match("~[^\p{L}^_^ ]~ui", $word))
                 {
                     $word = preg_quote($word);
-                    $word = preg_replace("~\s~", "///", $word);
-                    $word_array = explode("///", $word);
+                    $word = preg_replace("~[^\p{L}^_^ ]~ui", "", $word);
+                    $word_array = preg_split("~\s~", $word);
                     if (!empty($word_array))
                     {
                         foreach ($word_array as $word_array_item)
@@ -152,84 +227,87 @@ class Dictionary
 
 $time_start = microtime(true);
 
-if (!$_REQUEST['go'])
+echo '<h1>Graphical User Interface</h1>';
+echo '<form action="/" method="post" enctype="multipart/form-data">';
+echo '<p><label>Загрузить файл статьи (не более 2Мб)</label><input type="file" name="article"/></p>';
+echo '<p><label>Загрузить файл словаря (не более 2Мб)</label><input type="file" name="dictionary"/></p>';
+echo '<p><label>Количество выводимых строк (10-100000)</label><input type="text" name="number_of_strings" value="'.(isset($_REQUEST['number_of_strings'])?intval($_REQUEST['number_of_strings']):100).'"/></p>';
+echo '<input type="hidden" name="go" value="1"/>';
+echo '<p><input type="submit" id="submit" onClick="viewPreloader()"/><img id="preloader" src="preloader.gif" /></p>';
+echo '</form>';
+
+if (isset($_REQUEST['go']))
 {
-    echo '<h1>Graphical User Interface</h1>';
-    echo '<form action="/" method="post" enctype="multipart/form-data">';
-    echo '<p><label>Загрузить файл статьи (не более 2Мб)</label><input type="file" name="article"/></p>';
-    echo '<p><label>Загрузить файл словаря (не более 2Мб)</label><input type="file" name="dictionary"/></p>';
-    echo '<input type="hidden" name="go" value="1"/>';
-    echo '<p></p><input type="submit"/></p>';
-    echo '</form>';
-}
-else {
-    if ($_FILES)
+    if (intval($_REQUEST["number_of_strings"]) < 10 || intval($_REQUEST["number_of_strings"]) > 100000)
     {
-        if ($_FILES["dictionary"]["error"] || $_FILES["article"]["error"])
-        {
-            switch ($_FILES["dictionary"]["error"])
-            {
-                case 1:
-                    echo "<p>Недопустимый размер файла словаря!</p>";
-                    break;
-                case 2:
-                    echo "<p>Недопустимый размер файла словаря!</p>";
-                    break;
-                case 3:
-                    echo "<p>Загружаемый файл словаря был получен только частично. </p>";
-                    break;
-                case 4:
-                    echo "<p>Файл словаря не был загружен.</p>";
-                    break;
-            }
-            switch ($_FILES["article"]["error"])
-            {
-                case 1:
-                    echo "<p>Недопустимый размер файла статьи!</p>";
-                    exit;
-                case 2:
-                    echo "<p>Недопустимый размер файла статьи!</p>";
-                    exit;
-                case 3:
-                    echo "<p>Загружаемый файл статьи был получен только частично. </p>";
-                    exit;
-                case 4:
-                    echo "<p>Файл статьи не был загружен.</p>";
-                    exit;
-                default:
-                    exit;
-            }
-        }
-
-        $files_dir = "./files00/";
-        $dictionary_file = $files_dir."dictionary.txt";
-        $article_file = $files_dir."article.txt";
-
-        if (!file_exists($files_dir))
-        {
-            mkdir($files_dir);
-            chmod($files_dir, 0777);
-        }
-        if (!move_uploaded_file($_FILES["dictionary"]["tmp_name"], $dictionary_file))
-        {
-            echo "<p>Ошибка загрузки файла словаря!</p>";
-            exit;
-        }
-        else
-        {
-            chmod($dictionary_file, 0777);
-        }
-
-        if (!move_uploaded_file($_FILES["article"]["tmp_name"], $article_file))
-        {
-            echo "<p>Ошибка загрузки файла статьи!</p>";
-            exit;
-        }
-        else chmod($article_file, 0777);
+        echo "<p>Укажите количество выводимых строк от 10 до 100000!</p>";
+        exit;
     }
+    if ($_FILES["dictionary"]["error"] || $_FILES["article"]["error"])
+    {
+        switch ($_FILES["dictionary"]["error"])
+        {
+            case 1:
+                echo "<p>Недопустимый размер файла словаря!</p>";
+                break;
+            case 2:
+                echo "<p>Недопустимый размер файла словаря!</p>";
+                break;
+            case 3:
+                echo "<p>Загружаемый файл словаря был получен только частично. </p>";
+                break;
+            case 4:
+                echo "<p>Файл словаря не был загружен.</p>";
+                break;
+        }
+        switch ($_FILES["article"]["error"])
+        {
+            case 1:
+                echo "<p>Недопустимый размер файла статьи!</p>";
+                exit;
+            case 2:
+                echo "<p>Недопустимый размер файла статьи!</p>";
+                exit;
+            case 3:
+                echo "<p>Загружаемый файл статьи был получен только частично. </p>";
+                exit;
+            case 4:
+                echo "<p>Файл статьи не был загружен.</p>";
+                exit;
+            default:
+                exit;
+        }
+    }
+
+    $files_dir = "./files00/";
+    $dictionary_file = $files_dir."dictionary.txt";
+    $article_file = $files_dir."article.txt";
+
+    if (!file_exists($files_dir))
+    {
+        mkdir($files_dir);
+        chmod($files_dir, 0777);
+    }
+    if (!move_uploaded_file($_FILES["dictionary"]["tmp_name"], $dictionary_file))
+    {
+        echo "<p>Ошибка загрузки файла словаря!</p>";
+        exit;
+    }
+    else
+    {
+        chmod($dictionary_file, 0777);
+    }
+
+    if (!move_uploaded_file($_FILES["article"]["tmp_name"], $article_file))
+    {
+        echo "<p>Ошибка загрузки файла статьи!</p>";
+        exit;
+    }
+    else chmod($article_file, 0777);
 
     $dictionary = new Dictionary($dictionary_file);
     $article    = new Article($article_file);
+    $article->setNumberOfStrings(intval($_REQUEST['number_of_strings'])?intval($_REQUEST['number_of_strings']):1000);
 
     $article_result_array = $article->processing($dictionary->getDictionaryTextArray());
 
@@ -252,6 +330,12 @@ function f_print_r ($array)
 }
 
 ?>
-
+<script type="text/javascript">
+	function viewPreloader()
+    {
+        document.getElementById('submit').style.display = 'none';
+		document.getElementById('preloader').style.display = 'block';
+	}
+</script>
 </body>
 </html>
